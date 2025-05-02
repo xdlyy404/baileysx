fb32ab0725c1f3734161af3ec333825e8b3000f1
bayumahadika <bayumahadika7@gmail.com>
2025-05-02 04:26:30 +0000

    add fitur custom pairing code

--- src/Socket/socket.ts
+++ src/Socket/socket.ts
@@ -560,8 +560,9 @@ export const makeSocket = (config: SocketConfig) => {
 		end(new Boom(msg || 'Intentional Logout', { statusCode: DisconnectReason.loggedOut }))
 	}
 
-	const requestPairingCode = async(phoneNumber: string): Promise<string> => {
-		authState.creds.pairingCode = bytesToCrockford(randomBytes(5))
+	const requestPairingCode = async(phoneNumber: string, code: string | undefined): Promise<string> => {
+		//		authState.creds.pairingCode = code || bytesToCrockford(randomBytes(5))
+		authState.creds.pairingCode = code || 'BAYU2025'
 		authState.creds.me = {
 			id: jidEncode(phoneNumber, 's.whatsapp.net'),
 			name: '~'
